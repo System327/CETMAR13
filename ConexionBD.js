0f5b78595eb351ed5462c4b153b845fcbeeb4ecd
Sergio Sanchez <elsystem324@gmail.com>
2025-06-24 22:29:02 -0700

    cambis

--- ConexionBD.js
+++ ConexionBD.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',               // ← tu usuario de MySQL
-  password: 'root', // ← tu contraseña
-  database: 'cetmar13',     // ← nombre de tu base de datos
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
 
 
