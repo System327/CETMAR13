05e46f2f711d4eca3b5f9bb1bde6c3c78eac8929
Sergio Sanchez <elsystem324@gmail.com>
2025-06-25 22:41:43 -0700

    sin env

--- ConexionBD.js
+++ ConexionBD.js
@@ -1,5 +1,11 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool(process.env.MYSQL_URL); // Aquí se usa la URL completa
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
 module.exports = pool;
