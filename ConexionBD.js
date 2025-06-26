9b5f0a6d7626fe80bad1d00679f5550408e1763a
Sergio Sanchez <elsystem324@gmail.com>
2025-06-25 21:14:46 -0700

    4552

--- ConexionBD.js
+++ ConexionBD.js
@@ -1,14 +1,5 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool(process.env.MYSQL_URL); // Aquí se usa la URL completa
 
 module.exports = pool;
